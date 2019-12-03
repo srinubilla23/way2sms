fa8c223eeb4caddb77964f6be7f647a01c6fbcbb
srinubilla23 <srinu.billa23@gmail.com>
2019-12-03 17:07:27 +0530

    Added Language selection code for way2sms

--- Way2Sms/src/sendMessage/ToMobileNo.java
+++ Way2Sms/src/sendMessage/ToMobileNo.java
@@ -1,30 +1,27 @@
 package sendMessage;
 
-import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.swing.plaf.BorderUIResource.MatteBorderUIResource;
-
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.sikuli.script.FindFailed;
+import org.openqa.selenium.support.ui.Select;
 import org.sikuli.script.Screen;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
-import com.google.i18n.phonenumbers.NumberParseException;
-import com.google.i18n.phonenumbers.PhoneNumberUtil;
-import com.google.i18n.phonenumbers.PhoneNumberUtil.PhoneNumberFormat;
-import com.google.i18n.phonenumbers.Phonenumber.PhoneNumber;
 
 public class ToMobileNo 
 {
@@ -56,17 +53,12 @@ public class ToMobileNo
   public void link(String search) throws Exception
   {
 	  driver.get(search);
-	  Thread.sleep(2000);
-  }
-  
-  @Test(priority = 3) // This helps to minimize the Browser
-  public void minimize() throws Exception
-  { 
+	  Thread.sleep(1000);
 	  Screen mini=new Screen();
 	  mini.click("F:\\Download\\Images\\Bug Screenshot\\minimizeBrowser.JPG");
   }
-  
-  @Test(priority = 4) // Login Mobile and password
+    
+  @Test(priority = 3) // Login With way2sms Mobile Number and password
   public void validno() throws Exception
   { 
 	  System.out.println("Enter MObile Number: "); // Enter Mobile Number
@@ -87,57 +79,42 @@ public class ToMobileNo
 		  WebElement mobile=driver.findElement(By.id("mobileNo"));
 		  mobile.sendKeys(mob);
 	  }
-  }
 	  
-  @Test(priority = 5)
-  public void JavaRobot0() throws Exception
-	  {
-		  Thread.sleep(2000);
-		  Robot r=new Robot();
-		  r.keyPress(KeyEvent.VK_ALT);
-		  r.keyPress(KeyEvent.VK_TAB);
-		  r.keyRelease(KeyEvent.VK_ALT);
-		  r.keyRelease(KeyEvent.VK_TAB);
-		  Screen mini=new Screen();
-		  mini.click("F:\\Download\\Images\\Bug Screenshot\\minimizeBrowser.JPG");
-	  }
-	  
-  @Test(priority = 6)
-  public void way2password() throws Exception
-	  {
-		  Thread.sleep(2000);
-		  System.out.println("Enter password: ");  // Enter Password
-		  Scanner p1=new Scanner(System.in);
-		  String pass=p1.nextLine();
-		  WebElement password=driver.findElement(By.id("password"));
-		  password.sendKeys(pass);
-	  
-		  Thread.sleep(2000);
-		  Robot r=new Robot();
-		  r.keyPress(KeyEvent.VK_ALT);
-		  r.keyPress(KeyEvent.VK_TAB);
-		  r.keyRelease(KeyEvent.VK_ALT);
-		  r.keyRelease(KeyEvent.VK_TAB); 
-	  }
-  
-  @Test(priority = 7)
-  public void JavaRobot2() throws Exception
-  {
-	  Thread.sleep(3000);
-	  driver.findElement(By.xpath("//*[@id=\"loginform\"]/div[2]/div[2]/button")).click();     // Click on Login Button
-  }
-  
-  @Test(priority = 8)
-  public void minimize2() throws Exception
-  {
 	  Thread.sleep(2000);
+	  Robot r1=new Robot();
+	  r1.keyPress(KeyEvent.VK_ALT);
+	  r1.keyPress(KeyEvent.VK_TAB);
+	  r1.keyRelease(KeyEvent.VK_ALT);
+	  r1.keyRelease(KeyEvent.VK_TAB);
 	  Screen mini=new Screen();
 	  mini.click("F:\\Download\\Images\\Bug Screenshot\\minimizeBrowser.JPG");
+	  
+	  Thread.sleep(2000);
+	  System.out.println("Enter password: ");  // Enter Password
+	  Scanner p1=new Scanner(System.in);
+	  String pass=p1.nextLine();
+	  WebElement password=driver.findElement(By.id("password"));
+	  password.sendKeys(pass);
+
+	  Thread.sleep(1000);
+	  Robot r2=new Robot();
+	  r2.keyPress(KeyEvent.VK_ALT);
+	  r2.keyPress(KeyEvent.VK_TAB);
+	  r2.keyRelease(KeyEvent.VK_ALT);
+	  r2.keyRelease(KeyEvent.VK_TAB); 
+	  
+	  Thread.sleep(1000);
+	  driver.findElement(By.xpath("//*[@id=\"loginform\"]/div[2]/div[2]/button")).click();     // Click on Login Button
+	  Thread.sleep(2000);
+	  
   }
-  
-  @Test(priority = 9)
+ 
+  @Test(priority = 4) // Send SMS ( Sender Mobile Number and Enter Text Message )
   public void sendermobilenumber() throws Exception
   {
+	  Screen mini1=new Screen();
+	  mini1.click("F:\\Download\\Images\\Bug Screenshot\\minimizeBrowser.JPG"); 
+	  
 	  Thread.sleep(2000);
 	  System.out.println("Enter Sender MObile Number:"  );  // Enter sender Mobile Number
 	  Scanner sendnum=new Scanner(System.in);
@@ -169,7 +146,40 @@ public class ToMobileNo
 	  
 	  Thread.sleep(2000);
 	  driver.findElement(By.xpath("//*[@id=\"sendButton\"]")).click();
+	  Thread.sleep(2000);
+	  
+	  Screen mini3=new Screen();
+	  mini3.click("F:\\Download\\Images\\Bug Screenshot\\minimizeBrowser.JPG");
   }
+  
+  @Test(priority = 5) // 
+  public void languageSMS() throws Exception
+  {
+	  
+	  WebElement lang=driver.findElement(By.xpath("//*[contains (text(), 'Language SMS')]"));
+	  lang.click();
+	  Thread.sleep(2000);
+	  
+	  Screen mini1=new Screen();
+	  mini1.click("F:\\Download\\Images\\Bug Screenshot\\minimizeBrowser.JPG");
+	  
+	  Thread.sleep(2000);
+	  System.out.println("Enter Sender MObile Number:"  );  // Enter sender Mobile Number
+	  Scanner sendnum=new Scanner(System.in);
+	  String mobnum=sendnum.nextLine();
+	  driver.findElement(By.id("mobile")).sendKeys(mobnum);
+	  
+	  Thread.sleep(2000);
+	  Robot r2=new Robot();
+	  r2.keyPress(KeyEvent.VK_ALT);
+	  r2.keyPress(KeyEvent.VK_TAB);
+	  r2.keyRelease(KeyEvent.VK_ALT);
+	  r2.keyRelease(KeyEvent.VK_TAB);
+	  
+	  Select drop=new Select(driver.findElement((By.xpath("//*[@id=\"lanDiv\"]/select"))));
+	  drop.equals("Telugu");
+	  drop.selectByIndex(1);
+	    }
   
   
 }
